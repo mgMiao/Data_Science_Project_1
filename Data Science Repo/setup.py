6e2525c25a03741e9ccf2ac3fd8781b0638d292d
mgMiao <miaojie1228@gmail.com>
2020-02-03 22:29:09 -0600

    using cookiecutter
    first push

---
+++ Data Science Repo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a data sicence project about investigating the factors of stock price in the U.S. stock markets.',
+    author='Jie Miao',
+    license='',
+)
